b0840713ccfaff9c15c0f3005b26af764764eb33
Greg Doud <gpdoud@gmail.com>
2023-02-16 10:40:48 -0500

    Add project files.

---
+++ polymorphic-pets/Pet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphic_pets;
+internal class Pet {
+
+    public virtual void Speak() {
+        Console.WriteLine("NO SOUND!");
+    }
+
+}
+
+internal class Dog : Pet {
+
+    public override void Speak() {
+        Console.WriteLine("Bark!");
+    }
+
+}
+
+internal class Cat : Pet {
+
+    public override void Speak() {
+        Console.WriteLine("Meow!");
+    }
+
+}
---
+++ polymorphic-pets/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using polymorphic_pets;
+
+Pet p1 = new Pet();
+
+Dog d1 = new Dog();
+
+Cat c1 = new Cat();
+
+Pet[] pets = new Pet[] { d1, c1 };
+
+foreach(Pet pet in pets) {
+    pet.Speak();
+}
